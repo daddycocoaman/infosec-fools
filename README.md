95e0c83b182b13b90770985985a9dcc22565518a
Leron Gray <daddycocoaman@gmail.com>
2022-06-23 16:30:59 -0700

    push the fools

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # infosec-fools
-Are you going to Hacker Summer Camp, you fools?
+Are you going to Hacker Summer Camp, [you fools](https://twitter.com/kim_crawley/status/1539701262802878467)?
+
+`pipx install infosec-fools`
---
+++ infosec_fools/main.py
@@ -0,0 +1,29 @@
+from turtle import color
+import typer
+from rich import print
+from rich.prompt import Confirm
+from rich.highlighter import Highlighter
+from random import randint
+
+app = typer.Typer(
+    name="infosec_fools", help="CLI tool to determine if you are an infosec fool"
+)
+
+
+@app.command()
+def main():
+    prompt = Confirm.ask(
+        ":question_mark: Hi, are you going to Hacker Summer Camp 2022?"
+    )
+    if prompt:
+        print(
+            ":clown_face: :clown_face: :clown_face: [yellow]You are an infosec fool![/yellow] :clown_face: :clown_face: :clown_face:\n"
+        )
+    else:
+        print(
+            ":crown: :crown: :crown: [green]You are not an infosec fool![/green] :crown: :crown: :crown:"
+        )
+
+
+if __name__ == "__main__":
+    app()
